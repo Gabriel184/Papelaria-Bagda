f908e910e912e5fa8dbcfb4afc4db06ff4a677dd
Gabriel <gabriel.oliveira184@fatec.sp.gov.br>
2022-02-13 09:00:48 -0300

    Inicio da página inicial

--- FRONT_END/papelaria-bagda/src/App.tsx
+++ FRONT_END/papelaria-bagda/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Home from "./pages/Home";
+import "./index.css";
+
 
 function App() {
   return (
     <div>
+      <Home/>
     </div>
   );
 }
---
+++ FRONT_END/papelaria-bagda/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export default function Header(){
+    return (
+        <div className={"header-base"}>
+            <div className={"header-body"}>
+                <strong>TESTE</strong>
+            </div>
+        </div>
+    );
+}
